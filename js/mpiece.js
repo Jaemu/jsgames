e34a00041f0993919e24b46a51d659eb777e8fa9
Jill <jillian.munson@stonybrook.edu>
2014-08-10 13:32:44 -0400

    render semi-transparent circles over picture

--- js/mpiece.js
+++ js/mpiece.js
@@ -1,10 +1,14 @@
+/*
+To do -- make a scratcher game.  
+*/
 var MPiece = (function($, createjs){
 	
 		var defaults = {
 			thing : 'hi'
 		};
 
-		var carousel, stage, shape, domElement, particle;
+		var carousel, stage, shape, domElement, particle, painting;
+		var cover = [];
 		createjs.Ticker.timingMode = createjs.Ticker.RAF_SYNCHED;
 		createjs.Ticker.setFPS(30);
 
@@ -30,13 +34,31 @@ var MPiece = (function($, createjs){
 			canvas.height = $('.canvas-container').height();
 			stage = new createjs.Stage(canvas);
 			shape = new createjs.Shape();
-			particle = new createjs.Shape();
+			var i = 0;
+			for(i; i < canvas.height; i+=10){
+				cover.push([]);
+				var j = 0;
+				for(j; j < canvas.width; j+=10){
+					cover[i] = [];
+					var temp = new createjs.Shape();
+					temp.x = j;
+					temp.y = i;
+					temp.graphics.beginFill('rgba(255,0,0,.5)').drawCircle(0, 0, 10);
+					cover[i][j] = temp;
+					stage.addChild(temp);
+				}
+			}
+			
+			console.log('num children: ' + stage.getNumChildren());
+			
+			stage.update();
+			/*particle = new createjs.Shape();
 
 			particle.x = 0;
 			particle.y = 50;
 			particle.graphics.beginFill('rgba(255,0,0,1)').drawCircle(0, 0, 40);
 			stage.addChild(particle);
-
+			*/
 			createjs.Ticker.on("tick", tick);
 		};
 		var handleClick = function(e){
@@ -55,8 +77,8 @@ var MPiece = (function($, createjs){
 		};
 
 		var tick = function(e){
-			particle.x = particle.x + 10;
-			if (particle.x > stage.canvas.width) { particle.x = 0; }
+			//particle.x = particle.x + 10;
+			//if (particle.x > stage.canvas.width) { particle.x = 0; }
 			stage.update(e);
 		};
 

8974205afe4de2c1ce55266fe7e97a5ec09558e3
Jill <jillian.munson@stonybrook.edu>
2014-08-09 21:54:45 -0400

    no more blurry dot! (dynamically resize the canvas based on size of container)

--- js/mpiece.js
+++ js/mpiece.js
@@ -4,27 +4,40 @@ var MPiece = (function($, createjs){
 			thing : 'hi'
 		};
 
-		var carousel, stage, shape, domElement;
-		canvas = document.getElementById('canvas');
-		stage = new createjs.Stage(canvas);
-		shape = new createjs.Shape();
+		var carousel, stage, shape, domElement, particle;
+		createjs.Ticker.timingMode = createjs.Ticker.RAF_SYNCHED;
+		createjs.Ticker.setFPS(30);
+
 
 		var init = function(params){
 			defaults = $.extend(defaults, params, {});
 
-			$('#carousel').on('mouseup', this.handleClick)
+			/*$('#carousel').on('mouseup', this.handleClick)
 			.on('mouseover', clearCanvas)
 			.on('mouseout', handleClick);
-
-			$(window).on('ready', this.onLoad);
+			*/
+			$(document).on('ready', onLoad);
 		};
 
-		var onLoad = function(){
-			carousel = document.getElementById('carousel');
-			domElement = new createjs.DOMElement(carousel);
-			shape.graphics.beginFill('rgba(255,255,0,1)').drawRoundRect(0, 0, 120, 120, 10);
+		var onLoad = function(e){
+			/*shape.graphics.beginFill('rgba(255,255,0,1)').drawRoundRect(0, 0, 120, 120, 10);
 			stage.addChild(shape);
 			stage.update();
+			*/
+
+			canvas = document.getElementById('canvas');
+			canvas.width = $('.canvas-container').width();
+			canvas.height = $('.canvas-container').height();
+			stage = new createjs.Stage(canvas);
+			shape = new createjs.Shape();
+			particle = new createjs.Shape();
+
+			particle.x = 0;
+			particle.y = 50;
+			particle.graphics.beginFill('rgba(255,0,0,1)').drawCircle(0, 0, 40);
+			stage.addChild(particle);
+
+			createjs.Ticker.on("tick", tick);
 		};
 		var handleClick = function(e){
 			stage.removeAllChildren();
@@ -41,10 +54,15 @@ var MPiece = (function($, createjs){
 			stage.update();
 		};
 
+		var tick = function(e){
+			particle.x = particle.x + 10;
+			if (particle.x > stage.canvas.width) { particle.x = 0; }
+			stage.update(e);
+		};
+
 	return {
 		init: init
 	};
 
 })(jQuery, createjs);
-
 MPiece.init();
